800c8f5c637645946da5f0ddb5f02603f5bd46a5
Prynss <prynss_lp@hotmail.com>
2022-04-11 20:24:29 -0300

    add: click event on menu

--- main.js
+++ main.js
@@ -1,53 +1,15 @@
-(function(){
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toogle')
+const links = document.querySelectorAll('nav ul li a')
 
-    var doc = document.documentElement;
-    var w = window;
-  
-    var prevScroll = w.scrollY || doc.scrollTop;
-    var curScroll;
-    var direction = 0;
-    var prevDirection = 0;
-  
-    var header = document.getElementById('site-header');
-  
-    var checkScroll = function() {
-  
-      /*
-      ** Find the direction of scroll
-      ** 0 - initial, 1 - up, 2 - down
-      */
-  
-      curScroll = w.scrollY || doc.scrollTop;
-      if (curScroll > prevScroll) { 
-        //scrolled up
-        direction = 2;
-      }
-      else if (curScroll < prevScroll) { 
-        //scrolled down
-        direction = 1;
-      }
-  
-      if (direction !== prevDirection) {
-        toggleHeader(direction, curScroll);
-      }
-      
-      prevScroll = curScroll;
-    };
-  
-    var toggleHeader = function(direction, curScroll) {
-      if (direction === 2 && curScroll > 78) { 
-        
-        //replace 52 with the height of your header in px
-  
-        header.classList.add('hide');
-        prevDirection = direction;
-      }
-      else if (direction === 1) {
-        header.classList.remove('hide');
-        prevDirection = direction;
-      }
-    };
-    
-    window.addEventListener('scroll', checkScroll);
-  
-  })();
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
