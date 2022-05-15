f422732d98e889b04ec60ab7fd368f7d5037ddc8
Prynss <prynss_lp@hotmail.com>
2022-05-15 20:25:48 -0300

    add: tests with header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     colors: {
       'body': '#17171F',
       'selected-text': '#87021B',
-      'theme': '#E10234'
+      'theme': '#E10234',
+      'white': '#ffffff',
+      'nav': '#050607',
     },
     fontFamily: {
-      'poppins': ["'Poppins'", 'sans-serif']
+      'title': ["'Orbitron'", 'sans-serif'],
+      'body': ["'Saira Condensed'", 'sans-serif']
+    },
+    backgroundImage: {
+     'portait': "url('/assets/bg-hero.jpg')",
+     'sun': "url('/assets/redsun.png')"
     }
   },
   plugins: [],
