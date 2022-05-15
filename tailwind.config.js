455ec1de4a2fa446b08a1948989a650cd7fbbc1e
Prynss <prynss_lp@hotmail.com>
2022-05-15 14:17:32 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html}"],
+  theme: {
+    colors: {
+      'body': '#17171F',
+      'selected-text': '#87021B',
+      'theme': '#E10234'
+    },
+    fontFamily: {
+      'poppins': ["'Poppins'", 'sans-serif']
+    }
+  },
+  plugins: [],
+}
