77fc5dfd77f0ced5ab3493b7c1cb6dce4a0d7505
Prynss <prynss_lp@hotmail.com>
2022-06-23 21:22:49 -0300

    feat: :tada: initial commit
    Create initial sctruture using react, vite and tailwind.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-black font-text">
+      <h1 className="text-white">HELLO, PORTFOLIO PROJECT</h1>
+      <p className="text-white">- Prynss Maia</p>
+    </div>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  content: ["./src/**/*.{html}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        'primary':'#350207'
+        primary: '#350207',
       },
       fontFamily: {
-        'text': ["'Libre Franklin'", 'sans-serif']
+        text: ["'Libre Franklin'", 'sans-serif'],
       },
       backgroundImage: {
-       'portait': "url('/assets/bg-hero.jpg')",
-       'hero': "linear-gradient(121.36deg, #D7057F 9%, #C8098A 14%, #1442DB 38%)"
+        portait: "url('/assets/bg-hero.jpg')",
+        hero: 'linear-gradient(121.36deg, #D7057F 9%, #C8098A 14%, #1442DB 38%)',
       },
       boxShadow: {
-        'pink': '0px 0px 32px #D7057F85',
-        'red': '0px 0px 32px #BE09C880',
-        'blue': '0px 0px 24px #1442DB70'
-      }
+        pink: '0px 0px 32px #D7057F85',
+        red: '0px 0px 32px #BE09C880',
+        blue: '0px 0px 24px #1442DB70',
+      },
     },
   },
   plugins: [],
-}
+};
