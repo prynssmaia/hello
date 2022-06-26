031b685c29cea4c325306519b52679aa6653b6d1
Prynss <prynss_lp@hotmail.com>
2022-06-25 21:27:41 -0300

    fix: :art: change video to image

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 function BackgroundVideo() {
   return (
     <div className="absolute inset-0 -z-10">
-      <video
+      <img src="./assets/bg-hero.png" alt="" srcset="" />
+      {/* <video
         loop
         muted
+        autoplay
         playsinline
         className="h-full w-full object-cover"
         src="./assets/hello.mp4"
-      ></video>
+      ></video> */}
     </div>
   );
 }
