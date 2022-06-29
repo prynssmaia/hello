5d10a1815683999b262310b6ba6ff711ff519ae3
Prynss <prynss_lp@hotmail.com>
2022-06-28 23:31:26 -0300

    feat: :art: update video class

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 function BackgroundVideo() {
   return (
-    <div className="absolute inset-0 -z-10">
-      <img src="./assets/bg-hero.png" alt="" srcset="" />
-      {/* <video
+    <div className="absolute inset-0 -z-10 h-full w-full object-cover">
+      {/* <img src="./src/assets/bg-hero.png" alt="" srcset="" /> */}
+      <video
         loop
         muted
-        autoplay
+        autoPlay
         playsinline
         className="h-full w-full object-cover"
-        src="./assets/hello.mp4"
-      ></video> */}
+        src="./src/assets/hello.mp4"
+      ></video>
     </div>
   );
 }
