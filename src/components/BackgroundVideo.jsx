e47c1a4ad8c0cd8ddabe825bbe1683a8cd25fd20
prynssmaia <prynss_lp@hotmail.com>
2022-06-29 10:07:10 -0300

    feat: :art: update folder structure

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import video from '../assets/hello.mp4'
+import image from '../assets/bg-hero.png'
 
 function BackgroundVideo() {
   return (
     <div className="absolute inset-0 -z-10 h-full w-full object-cover">
-      {/* <img src="./src/assets/bg-hero.png" alt="" srcset="" /> */}
+      {/* <img src={image} alt="" srcset="" /> */}
       <video
         loop
         muted
         autoPlay
         playsinline
         className="h-full w-full object-cover"
-        src="./src/assets/hello.mp4"
+        src={video}
       ></video>
     </div>
   );
