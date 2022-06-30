56391868e133a9ee4b5bb5898453d664f05d13f3
prynssmaia <prynss_lp@hotmail.com>
2022-06-30 13:35:23 -0300

    change assets to public folder

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import video from '../assets/hello.mp4'
-import image from '../assets/bg-hero.png'
+import video from '/assets/hello.mp4'
+import image from '/assets/bg-hero.png'
 
 function BackgroundVideo() {
   return (
