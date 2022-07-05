049fdcd0adfa6665e517eb14c1725913787b3ded
prynssmaia <prynss_lp@hotmail.com>
2022-07-05 13:14:48 -0300

    feat: :art: create projects page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './containers/Home';
+import Projects from './containers/Projects';
 
 function App() {
   return (
     <div className="min-h-screen bg-home">
       <Home />
+      <Projects />
     </div>
   );
 }
---
+++ src/containers/Projects.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import TitleGradient from '../components/TitleGradient'
+
+function Projects() {
+  return (
+    <div className="container min-h-screen" id="projects">
+      <div className='grid grid-cols-1 md:grid-cols-2 py-6'>
+
+        <TitleGradient text="Projects"/>
+        <div className='grid gap-7 md:gap-10'>
+          <div className=''>
+            <img src="https://images.unsplash.com/photo-1514830902516-48e20ae0ced9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"/>
+          </div>
+          <div className=''>
+            <img src="https://images.unsplash.com/photo-1518834984017-faefb4572688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"/>
+          </div>
+          <div className=''>
+            <img src="https://images.unsplash.com/photo-1612170482263-b7e393c4e87f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
