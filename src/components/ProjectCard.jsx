fc0f7dcc54865502cac6cc1a1d6984d35a1d9498
Prynss <prynss_lp@hotmail.com>
2022-07-06 21:08:05 -0300

    refactor: :art: improve project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 
 function ProjectCard({ title, description }) {
   return (
-    <div className='grid relative group'>
-      {/* card content */}
-      <div >
-        <div className='absolute group-hover:-inset-0.5 group-hover:bg-hero group-hover:blur-md group-hover:opacity-40'></div>
-        {/* background image */}
-        <div className='relative'>
-              <img src="https://images.unsplash.com/photo-1514830902516-48e20ae0ced9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"/>
-        </div>
-        {/* project info */}
-        <div className='grid gap-4 font-text pt-10 p-4 text-white bg-blk absolute bottom-0 w-full opacity-0 group-hover:opacity-100 transition duration-700 group-hover:duration-200'>
-          {/* titulo */}
-          <h3 className='font-bold text-xl lg:text-2xl'>{title}</h3>
-          {/* text */}
-          <p className='min-w-full text-sm lg:text-base'>{description}</p>
-        </div>
+    <div className="group relative grid w-full cursor-pointer rounded">
+      {/* background image */}
+      <div className="rounded">
+        <img
+          className="h-60 w-full rounded object-cover"
+          src="https://images.unsplash.com/photo-1514830902516-48e20ae0ced9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+        />
+      </div>
+      {/* project info */}
+      <div className="absolute bottom-0 grid w-full justify-start gap-4 rounded-b bg-blk p-4 pt-10 font-text text-white opacity-100 transition duration-700 group-hover:opacity-100 group-hover:duration-200 md:opacity-0">
+        {/* titulo */}
+        <h3 className="text-xl font-bold lg:text-2xl">{title}</h3>
+        {/* text */}
+        <p className="min-w-full text-sm lg:text-base">{description}</p>
       </div>
     </div>
   );
