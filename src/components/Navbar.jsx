a2333ce3424a230d911bb4907a35beb7d23ac001
prynssmaia <prynss_lp@hotmail.com>
2022-07-26 15:38:20 -0300

    fix: :bug: remove tooltips

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import ReactTooltip from "react-tooltip";
 import { NavigationArrow, Layout, TextT, ChatCircle, List, X } from 'phosphor-react';
 
 function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
-  const [tooltip, showTooltip] = useState(true);
 
   return (
     <header className="sticky top-0 z-20 bg-black10 border-b border-[#444444]">
@@ -44,31 +42,20 @@ function Navbar() {
               <NavigationArrow size={32} color="#fafcfc" weight="thin" />
             </a>
           </li>
-          <li className="h-full p-2 duration-300 transition-all hover:bg-black20" data-tip data-for="projectsTip">
+          <li className="h-full p-2 duration-300 transition-all hover:bg-black20">
             <a className="cursor-point" href="#projects">
               <Layout size={32} color="#fafcfc" weight="thin" />
             </a>
-            {tooltip && <ReactTooltip effect="solid" place="bottom" />}
-            <p
-              data-tip="hello world"
-              onMouseEnter={() => showTooltip(true)}
-              onMouseLeave={() => {
-                showTooltip(false);
-                setTimeout(() => showTooltip(true), 50);
-              }}
-            />
           </li>
-          <li className="h-full p-2 duration-300 transition-all hover:bg-black20" data-tip data-for="aboutTip">
+          <li className="h-full p-2 duration-300 transition-all hover:bg-black20">
             <a className="cursor-point" href="#about">
               <TextT size={32} color="#fafcfc" weight="thin" />
             </a>
-            <ReactTooltip id="aboutTip" place="bottom" effect="solid">Sobre</ReactTooltip>
           </li>
-          <li className="h-full p-2 duration-300 transition-all hover:bg-black20" data-tip data-for="contactTip">
+          <li className="h-full p-2 duration-300 transition-all hover:bg-black20">
             <a className="cursor-point" href="#contact">
               <ChatCircle size={32} color="#fafcfc" weight="thin" />
             </a>
-            <ReactTooltip id="contactTip" place="bottom" effect="solid">Contato</ReactTooltip>
           </li>
         </ul>
         {/* menu-mobile */}
