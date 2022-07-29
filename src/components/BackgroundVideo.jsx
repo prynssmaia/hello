b723af8463fcc63f1ff9f21480d474d085c0d8a6
Prynss <prynss_lp@hotmail.com>
2022-07-28 23:01:11 -0300

    style: :lipstick: add hello card

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -4,14 +4,20 @@ import image from '/assets/bg-hero.png';
 
 function BackgroundVideo() {
   return (
-    <div className="absolute inset-0 -z-10 h-screen object-cover">
+    <>
       {/* <img src={image} alt="" srcset="" /> */}
-      <video loop muted autoPlay playsInline className="">
+      <video
+        loop
+        muted
+        autoPlay
+        playsInline
+        className="inset-0 -z-10 h-full w-full rounded object-cover"
+      >
         <source src={video} type="video/mp4; codecs=hvc1" />
         <source src={video} type="video/webm; codecs=vp9" />
         <source src={video} type="video/ogg" />
       </video>
-    </div>
+    </>
   );
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import TitleGradient from '../components/TitleGradient';
-import ProjectCard from '../components/ProjectCard';
+import BackgroundVideo from '../components/BackgroundVideo';
 import { InstagramLogo, GithubLogo, LinkedinLogo } from 'phosphor-react';
 
 function Home() {
@@ -12,7 +12,13 @@ function Home() {
         <div className="grid auto-cols-fr grid-cols-1 grid-rows-1 gap-4 overflow-hidden md:grid-cols-2">
           <TitleGradient text="Welcome" />
           <div className="grid gap-7 md:col-span-2 md:col-start-2 md:gap-10">
-            {/* <div className=" grid gap-7 md:gap-10"> */}
+            {/* card hello */}
+            <div className="relative h-32 w-80 rounded bg-home">
+              <BackgroundVideo />
+              <div className="absolute bottom-0 left-0 px-2">
+                <TitleGradient text="Hello." />
+              </div>
+            </div>
 
             {/* card lucky number */}
             <div className="group flex h-32 w-80 flex-col justify-between rounded bg-[#FFD171] p-2">
@@ -26,35 +32,46 @@ function Home() {
               </p>
             </div>
 
-            {/* social links */}
-            <div className="flex items-center h-32 w-80 rounded bg-[#F86631] py-4 px-8">
-              <ul className='flex justify-between w-full'>
+            {/* card social links */}
+            <div className="flex h-32 w-80 items-center rounded bg-[#F86631] py-4 px-8">
+              <ul className="flex w-full justify-between">
                 <div>
-                  <a className="cursor-point" href="https://www.instagram.com/prynssmaia/" target='_blank'>
-                    <li className='absolute flex items-center justify-center transition duration-200 hover:-translate-y-1 hover:-translate-x-1 h-16 w-16 rounded-full border-2 border-[#171717] bg-[#DB438A]'>
-                        <InstagramLogo size={32} color="#171717" />
+                  <a
+                    className="cursor-point"
+                    href="https://www.instagram.com/prynssmaia/"
+                    target="_blank"
+                  >
+                    <li className="absolute flex h-16 w-16 items-center justify-center rounded-full border-2 border-[#171717] bg-[#DB438A] transition duration-200 hover:-translate-y-1 hover:-translate-x-1">
+                      <InstagramLogo size={32} color="#171717" />
                     </li>
                   </a>
-                  <div className='h-16 w-16 bg-white rounded-full'></div>
+                  <div className="h-16 w-16 rounded-full bg-white"></div>
                 </div>
 
                 <div>
-                <a className="cursor-point" href="https://github.com/prynssmaia" target='_blank'>
-                  <li className='absolute flex items-center justify-center transition duration-200 hover:-translate-y-1 hover:-translate-x-1 h-16 w-16 rounded-full border-2 border-[#171717] bg-[#12BF4F]'>
-                    <GithubLogo size={32} color="#171717" />
-                  </li>
-                  <div className='h-16 w-16 bg-white rounded-full'></div>
-                </a>
+                  <a
+                    className="cursor-point"
+                    href="https://github.com/prynssmaia"
+                    target="_blank"
+                  >
+                    <li className="absolute flex h-16 w-16 items-center justify-center rounded-full border-2 border-[#171717] bg-[#12BF4F] transition duration-200 hover:-translate-y-1 hover:-translate-x-1">
+                      <GithubLogo size={32} color="#171717" />
+                    </li>
+                    <div className="h-16 w-16 rounded-full bg-white"></div>
+                  </a>
                 </div>
-                
 
                 <div>
-                <a className="cursor-point" href="https://www.linkedin.com/in/prynss-maia-da-silva-689866241/" target='_blank'>
-                  <li className='absolute flex items-center justify-center transition duration-200 hover:-translate-y-1 hover:-translate-x-1 h-16 w-16 rounded-full border-2 border-[#171717] bg-[#0F7BFF]'>
-                    <LinkedinLogo size={32} color="#171717" />
-                  </li>
-                  <div className='h-16 w-16 bg-white rounded-full'></div>
-                </a>
+                  <a
+                    className="cursor-point"
+                    href="https://www.linkedin.com/in/prynss-maia-da-silva-689866241/"
+                    target="_blank"
+                  >
+                    <li className="absolute flex h-16 w-16 items-center justify-center rounded-full border-2 border-[#171717] bg-[#0F7BFF] transition duration-200 hover:-translate-y-1 hover:-translate-x-1">
+                      <LinkedinLogo size={32} color="#171717" />
+                    </li>
+                    <div className="h-16 w-16 rounded-full bg-white"></div>
+                  </a>
                 </div>
               </ul>
             </div>
