8dd54534c607e1da0843531e9f23036512fceccb
Prynss <prynss_lp@hotmail.com>
2023-03-05 00:43:55 -0300

    fix: :bug: adjust gradient height

--- src/components/FrameBackground.jsx
+++ src/components/FrameBackground.jsx
@@ -4,7 +4,7 @@ import styles from '../style';
 function FrameBackground({ children }) {
   return (
     <div className="container-canva solid padding bg-grid-pattern flex h-screen flex-col border border-black0">
-      <div className="blk-gradient absolute inset-0 opacity-90"></div>
+      <div className="blk-gradient absolute inset-0 h-screen opacity-90"></div>
       {children}
     </div>
   );
