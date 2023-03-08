b9ea91bc1a6f42296145914ccd2a217b586be88b
Prynss <prynss_lp@hotmail.com>
2023-03-07 23:19:23 -0300

    style: :lipstick: update background color

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,14 +4,12 @@ import styles from '../style';
 const Profile = () => {
   return (
     <div className="pt-[65px]">
-      <p
-        className={`${styles.flexCenter} h-12 w-12 rounded-full border border-white10 bg-blue10 text-xl`}
-      >
+      <p className={`${styles.flexCenter} h-12 w-12 rounded-full bg-primary text-xl`}>
         P
       </p>
       <div className="py-6">
         <h1 className="text-xl">Prynss Maia da Silva</h1>
-        <a href="mailto:prynssmaia@gmail.com" className="text-base text-blue10">
+        <a href="mailto:prynssmaia@gmail.com" className="text-base text-primary">
           prynssmaia@gmail.com
         </a>
       </div>
