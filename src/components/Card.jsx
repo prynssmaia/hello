57911cd1800b7559db2aa28718f8f7a7aa3c3aca
Prynss <prynss_lp@hotmail.com>
2023-03-11 00:22:00 -0300

    refactor: :recycle: remove unnecessary div

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,13 @@ function Card({ children, width, height, flex, bgColor, cursor }) {
     // <div className= card z-50 text-white`}>
     //   <div className="card-content flex-center">{(children, text)}</div>
     // </div>
-    <div className="z-10 mb-6">
+    <div
+      className={`${width} ${height} ${flex} ${cursor} border-radius z-10 bg-gradient-to-br from-black0 p-[1px]`}
+    >
       <div
-        className={`${width} ${height} ${flex} ${cursor} border-radius bg-gradient-to-br from-black0 p-[1px]`}
+        className={`${flex} border-radius padding h-full w-full space-y-2 ${bgColor} md:space-y-4`}
       >
-        <div
-          className={`${flex} border-radius padding h-full w-full space-y-2 ${bgColor} md:space-y-4`}
-        >
-          {children}
-        </div>
+        {children}
       </div>
     </div>
   );
