37ee32c76cf6cc7c53a5df2c4aab2b1dd5113b77
Prynss <prynss_lp@hotmail.com>
2023-03-11 00:23:18 -0300

    refactor: :recycle: change grow axis

--- src/components/FrameBackground.jsx
+++ src/components/FrameBackground.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FrameBackground({ children }) {
   return (
-    <div className="container-canva flex-grow-h bg-grid-pattern solid padding z-10 border border-black0">
+    <div className="container-canva flex-grow-y bg-grid-pattern solid padding z-10 border border-black0">
       <div className="blk-gradient absolute inset-0 h-screen opacity-90"></div>
       {children}
     </div>
