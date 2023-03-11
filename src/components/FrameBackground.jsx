577acb4f8e74dbad0af7b1b3a5dc5f1b3030dcd1
Prynss <prynss_lp@hotmail.com>
2023-03-10 22:04:46 -0300

    refactor: :recycle: update vertical alignment of main element

--- src/components/FrameBackground.jsx
+++ src/components/FrameBackground.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FrameBackground({ children }) {
   return (
-    <div className="container-canva solid padding bg-grid-pattern flex h-screen flex-col border border-black0">
+    <div className="container-canva flex-grow-h bg-grid-pattern solid padding z-10 border border-black0">
       <div className="blk-gradient absolute inset-0 h-screen opacity-90"></div>
       {children}
     </div>
