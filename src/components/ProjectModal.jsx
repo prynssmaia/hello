13c4e972a6125a784f4701f917923bea9922bee2
Prynss <prynss_lp@hotmail.com>
2023-03-15 22:42:48 -0300

    fix: :art: adjust close icon modal position

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Card from './Card';
-import { Tag } from '../components';
+import { X } from 'phosphor-react';
 import { projects } from '../constants';
 
 function ProjectModal({ isOpen, onClose, children }) {
@@ -12,7 +11,16 @@ function ProjectModal({ isOpen, onClose, children }) {
       ></div>
       <div className="border-radius fixed top-auto right-1/2 z-[51] translate-x-1/2 translate-y-1/2 bg-gradient-to-br from-black0 p-[1px]">
         <div className="border-radius h-full w-full bg-black19">
-          <div className="border-radius relative h-full w-full overflow-hidden pb-48">
+          <div className="relative h-full w-full overflow-hidden rounded-t-md pb-48 md:rounded-t-lg xl:rounded-t-[10px]">
+            <button
+              className="transition-basic border-radius group absolute right-2 top-2 z-10 bg-black20 p-1 hover:bg-black11"
+              onClick={onClose}
+            >
+              <X
+                className="h-4 w-4 text-white group-hover:text-white10"
+                color="#fafcfc"
+              />
+            </button>
             <img
               className="absolute inset-0 h-full w-full object-cover"
               src="https://source.unsplash.com/random/?red-aesthetic&1"
@@ -23,9 +31,6 @@ function ProjectModal({ isOpen, onClose, children }) {
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
               recusandae?
             </p>
-            <button className="bg-primary" onClick={onClose}>
-              Fechar
-            </button>
           </div>
         </div>
       </div>
