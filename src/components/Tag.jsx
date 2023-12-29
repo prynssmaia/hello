6ca7ed006714d8fc99e0db64f3de44ff2f6571be
Prynss <prynss_lp@hotmail.com>
2023-12-28 22:40:06 -0300

    refactor: :art: update font family

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Tag({ children, text }) {
   return (
     <div className="flex-start border-radius padding-tag z-40 w-fit border-[1px] border-solid border-abso-700/10 bg-abso-700/20 text-xs font-medium text-abso-700 md:text-sm xl:text-base">
-      {children}
+      <p>{children}</p>
     </div>
   );
 }
